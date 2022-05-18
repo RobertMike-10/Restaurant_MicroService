11da1c176529fa483ac1167ff2c392fff893af29
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2022-05-18 17:45:51 -0500

    MVC View Product Create

---
+++ DeliRestaurant/Restaurant.Web/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Restaurant.Web.Models;
+using Restaurant.Web.Services.IServices;
+
+namespace Restaurant.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> products = new();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>();
+            if (response != null && response.isSuccess)
+            {
+                products = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(products);
+
+        }
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductCreate(ProductDto product)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProductAsync<ResponseDto>(product);
+                if (response != null && response.isSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+
+                }
+            }
+            return View(product);
+        }
+    }
+}
